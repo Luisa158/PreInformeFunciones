2e3383f91baeb9b7b9ff42b3260ed453ef449d24
Luisa158 <luisa.monsalve.2019@upb.edu.co>
2019-03-28 19:51:29 -0500

    Impares

---
+++ impares.py
@@ -0,0 +1,25 @@
+
+x=int(input("Por favor, ngrese un número: "))
+def numeros_impares (x):
+
+ for i in range(0, x+1):
+    if i % 2 != 0:
+      print(i)
+
+numeros_impares(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
